4066198ba18cb14a84bf8c477264297d0b34a81c
Christopher Walle <cjwalle97@gmail.com>
2017-08-24 10:09:00 -0500

    Fixed Libraries
    Fixed Libraries

--- GraphicsAssessment/GraphicsAssessment/FlyCamera.cpp
+++ GraphicsAssessment/GraphicsAssessment/FlyCamera.cpp
@@ -14,11 +14,11 @@ FlyCamera::~FlyCamera()
 void SetLookAt(vec3 eye, vec3 center, vec3 up) 
 {
 	vec3 f = eye - center;
-	vec3 z = glm::normalize<vec3>(f);
+	vec3 z = glm::normalize<>(f);
 	vec3 s = glm::cross(up, z);
-	vec3 x = glm::normalize<vec3>(s);
+	vec3 x = glm::normalize<>(s);
 	vec3 u = glm::cross(z, x);
-	vec3 y = glm::normalize<vec3>(u);
+	vec3 y = glm::normalize<>(u);
 
 
 }
