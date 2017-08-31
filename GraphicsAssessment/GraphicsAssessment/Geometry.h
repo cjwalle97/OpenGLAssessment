99e53459e8d23b63d32afd45e7a8facbc4f31195
Christopher Walle <cjwalle97@gmail.com>
2017-08-31 15:03:07 -0500

    Update
    Update

--- GraphicsAssessment/GraphicsAssessment/Geometry.h
+++ GraphicsAssessment/GraphicsAssessment/Geometry.h
@@ -1,10 +1,27 @@
+#include <GLFW\glfw3.h>
+#include <stdio.h>
+#include <assert.h>
+#include <glm\vec3.hpp>
+#include <glm\vec2.hpp>
+#include <glm\glm.hpp>
+#include <glm\ext.hpp>
+#include <iostream>
+
+using glm::vec3;
+using glm::vec4;
+
 #pragma once
 class Geometry
 {
+private:
+	struct Vertex
+	{
+		vec4 position;
+		vec4 color;
+	};
 public:
 	Geometry();
 	~Geometry();
-
-
+	void generateGrid(unsigned int rows, unsigned int columns) {}
 };
 
